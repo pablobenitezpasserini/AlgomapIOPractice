37535c3d4eb820adbef70d33bbdc908756e608e5
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-03-19 14:49:23 -0300

    solucion 14LongestCommonPrefix

---
+++ ArraysAndStrings/14LongestCommonPrefix/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _14LongestCommonPrefix;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // link https://leetcode.com/problems/longest-common-prefix/description/
+    }
+    public static string LongestCommonPrefix(string[] strs) {
+
+        string shortest = strs.OrderBy(s => s.Length).First();
+
+        for(int i = 0; i < shortest.Length; i++)
+        {
+            if ( strs.Select( s => s[i]).Distinct().Count() > 1 )
+            {
+                return shortest[..i];
+            }
+        }
+        return shortest;
+    }
+}
