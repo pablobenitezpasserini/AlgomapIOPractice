2351e68cba01a14ff36e0e0e613dca0cd569a4da
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-03-19 14:14:44 -0300

    solucion 121BestTimeToBuyAndSellStock

---
+++ ArraysAndStrings/121BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace _121BestTimeToBuyAndSellStock;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // link https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+    }
+    public static int MaxProfit(int[] prices) {
+        int maxP = 0;
+        int l = 0;
+        int r = 1;
+        int profit = 0;
+
+        while(r < prices.Length)
+        {
+            if(prices[l] < prices[r])
+            {
+                profit = prices[r] - prices[l];
+                maxP = Math.Max(profit, maxP);
+            }
+            else
+            {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    }
+}
