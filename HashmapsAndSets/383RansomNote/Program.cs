2e2862a089857518c535c0536360125433c6dadd
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-03-20 23:50:49 -0300

    solucion 383RansomNote

---
+++ HashmapsAndSets/383RansomNote/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _383RansomNote;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // link: https://leetcode.com/problems/ransom-note/
+    }
+    public static bool CanConstruct(string ransomNote, string magazine)
+    {
+        Dictionary<char, int> lettersPool = new Dictionary<char, int>();
+
+        foreach (char letter in magazine)
+        {
+            if (lettersPool.ContainsKey(letter))
+            {
+                lettersPool[letter]++;
+            }
+            else
+            {
+                lettersPool.Add(letter, 1);
+            }
+        }
+
+        foreach (char letter in ransomNote)
+        {
+            if (lettersPool.ContainsKey(letter))
+            {
+                if (lettersPool[letter] == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    lettersPool[letter]--;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
